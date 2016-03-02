fda900809b3a9a16e422e3c4c7d83275f9479cac
Skylar King <sking@crazyawesome.io>
2016-03-01 22:19:39 -0600

    Add postkey validation. Update styling.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,8 @@ class PostsController < ApplicationController
     @post.title = params[:post][:title]
     @post.content = params[:post][:content]
     @post.created_at = DateTime.now.in_time_zone("Central Time (US & Canada)")
-    @post.save ? (redirect_to posts_path) : (render :addpost)
+    @post.created_by = params[:post][:created_by]
+    @post.postkey = params[:post][:postkey]
+    @post.save ? (redirect_to posts_path) : (render :_addpost)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, :content, :created_by, :postkey, presence: true
+  validates :postkey, inclusion: { in: %w(bankroll),
+    message: "must be valid to make this change." }
 end
