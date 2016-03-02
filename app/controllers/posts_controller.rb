1615c6a0a453bdf8aee410823d7d6e3570946028
Skylar King <sking@crazyawesome.io>
2016-03-01 21:06:44 -0600

    Update to work with partial.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,21 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def show
+    @post = Post.find_by id params[:id]
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.content = params[:post][:content]
+    @post.created_at = DateTime.now.in_time_zone("Central Time (US & Canada)")
+    @post.save ? (redirect_to posts_path) : (render :addpost)
   end
 end
