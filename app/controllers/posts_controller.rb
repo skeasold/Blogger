6e3379a335e13e93cb202f4b16285f70e4d83974
Skylar King <sking@crazyawesome.io>
2016-03-02 00:45:59 -0600

    Common styling between views. Add update, edit, delete within controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,26 @@ class PostsController < ApplicationController
     @post.created_at = DateTime.now.in_time_zone("Central Time (US & Canada)")
     @post.created_by = params[:post][:created_by]
     @post.postkey = params[:post][:postkey]
-    @post.save ? (redirect_to posts_path) : (render :_addpost)
+    @post.save ? (redirect_to posts_path) : (render :new)
+  end
+
+  def edit
+    @post = Post.find_by id: params[:id]
+  end
+
+  def update
+    @post = Post.find_by id: params[:id]
+    @post.title = params[:post][:title]
+    @post.content = params[:post][:content]
+    @post.created_at = DateTime.now.in_time_zone("Central Time (US & Canada)")
+    @post.created_by = params[:post][:created_by]
+    @post.postkey = params[:post][:postkey]
+    @post.save ? (redirect_to posts_path) : (render :edit)
+  end
+
+  def delete
+    @post = Post.find_by id: params[:id]
+    @post.destroy
+    redirect_to posts_path
   end
 end
