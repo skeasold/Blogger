d04468b3fa1129c26cc3f1049811978a9d972c03
Skylar King <sking@crazyawesome.io>
2016-03-01 20:21:43 -0600

    Switch order of route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   get '/' => 'posts#index', as: :posts
-  get '/:id' => 'posts#show', as: :post
   get '/new' => 'posts#new', as: :new_post
+  get '/:id' => 'posts#show', as: :post
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
